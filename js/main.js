c8613a7fcd4fd337a75d1bbe6eef793dd1b03f06
Tu Van Tran <trantugiang122899@gmail.com>
2020-07-31 11:15:31 +0700

    create basic front-end site with material css library

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+  $('.sidenav').sidenav();
+  $('.materialboxed').materialbox();
+  $('.parallax').parallax();
+  $('.tabs').tabs();
+  $('.datepicker').datepicker();
+  $('.tooltipped').tooltip();
+});
